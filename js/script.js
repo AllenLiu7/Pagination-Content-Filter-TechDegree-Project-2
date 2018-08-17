c7272456cb3b5eb85a0e10ee754241dae82bf823
AllenLiu7 <41953650+AllenLiu7@users.noreply.github.com>
2018-08-17 14:40:49 +0800

    Pagination Ver 1.1
    Exceeds Expectations Version

--- js/script.js
+++ js/script.js
@@ -4,10 +4,11 @@ FSJS project 2 - List Filter and Pagination
 ******************************************/
 
 
-// Add variables that store DOM elements you will need to reference and/or manipulate
+// Add variables that store DOM elements that will need to reference and/or manipulate
 const studentItems = document.getElementsByClassName("student-item cf");
 
-// Create a function to hide all of the items in the list excpet for the ten you want to show
+
+// Create a function to hide all of the items in the list excpet for the ten items to show
 const showPage = (page, list) => {
   for (let i=0; i<list.length; i+=1) {
     if (i >= page*10-10 && i < page*10) {
@@ -35,7 +36,7 @@ const appendPageLinks = (list) => {
     $('.pagination ul').append('<li><a href="#">' + i +'</a>');
   }
   // add event listener to a tag
-  $(".pagination").on("click", "a", function(event){
+  $('.pagination').on('click', 'a', function(event){
     showPage(parseInt($( this ).text()), list);
     // remove class then add new class to a
     $('a').each(function(){
@@ -47,3 +48,48 @@ const appendPageLinks = (list) => {
 
 showPage(1, studentItems)
 appendPageLinks(studentItems);
+
+
+// append input feild and button
+$('h2').append('<div class="student-search"></div>');
+$('.student-search').append('<input placeholder="Search for students...">');
+$('.student-search').append('<button>Search</button>');
+
+// event handler on click
+$('button').on('click', function(){
+  const search = $('input').val();
+  //loop through the list to find the student name that match the input
+  for (let i=0; i<studentItems.length; i+=1) {
+    if ($('.student-item').eq(i).find('h3').text() === search) {
+      //'block' the match one, and hide the ohters
+      studentItems[i].style.display = 'block';
+    } else {
+      studentItems[i].style.display = 'none';
+    }
+  }
+  // if there is no matches, display 'No results'
+  if ($("[style='display: block;']").length === 0) {
+    $('.student-list').append('<p>No results</p>');
+  }
+  // pagination based on the result list
+  if ($('div').hasClass('pagination'))  {
+    $('.pagination').remove();
+  }
+  appendPageLinks($("[style='display: block;']"))
+});
+
+// envent handler on 'keyup', similar to 'click'
+$('input').on('keyup', function(){
+  const search = $('input').val();
+  for (let i=0; i<studentItems.length; i+=1) {
+    if ($('.student-item').eq(i).find('h3').text() === search) {
+      studentItems[i].style.display = 'block';
+    } else {
+      studentItems[i].style.display = 'none';
+    }
+  }
+  if ($('div').hasClass('pagination'))  {
+    $('.pagination').remove();
+  }
+  appendPageLinks($("[style='display: block;']"))
+});
